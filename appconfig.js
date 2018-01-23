126fe4c1d445fb5f875a1c87141c5aadecfe7ddf
MikesWei <695034525@qq.com>
2018-01-23 23:48:36 +0800

    Add files via upload

---
+++ appconfig.js
@@ -0,0 +1,14 @@
+﻿var appConfig = {  
+    BaseURL: "/" 
+}
+window.CESIUM_BASE_URL = appConfig.BaseURL + "ThirdParty/Cesium/";
+if (typeof define === "function") {
+    define(function () {
+        return appConfig;
+    });
+} else if (typeof module === "undefined") {
+
+} else {
+    module.exports = appConfig;
+}
+window.appConfig = appConfig;
