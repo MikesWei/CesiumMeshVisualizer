2fb500d6abb57ce3b08dcb605d5bc208f8028fb0
MikesWei <695034525@qq.com>
2018-01-24 22:35:20 +0800

    Add files via upload

---
+++ Source/Core/MeshPhongMaterial.js
@@ -0,0 +1,23 @@
+﻿define([
+    'Core/MeshMaterial',
+    'Core/Shaders/phong_frag',
+    'Core/Shaders/phong_vert' 
+], function (
+    MeshMaterial,
+    phong_frag,
+    phong_vert 
+    ) {
+    /**
+    * 
+    *@constructor
+    *@memberof Cesium
+    *@extends Cesium.MeshMaterial
+    */
+    function MeshPhongMaterial(options) {
+        MeshMaterial.apply(this, arguments);
+        this.vertexShader = phong_vert;
+        this.fragmentShader = phong_frag;
+    }
+    MeshPhongMaterial.prototype = new MeshMaterial();
+    return MeshPhongMaterial;
+})
