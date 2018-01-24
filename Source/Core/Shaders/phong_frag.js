31e659e8a5e384d336c42bc8e05ef7d997f0e44e
MikesWei <695034525@qq.com>
2018-01-25 06:27:57 +0800

    Update phong_frag.js

--- Source/Core/Shaders/phong_frag.js
+++ Source/Core/Shaders/phong_frag.js
@@ -1,10 +1,13 @@
-﻿define(function () {
+define(function () {
     var phong_frag = '\n\
 varying vec3 v_position;\n\
 varying vec3 v_normal;\n\
 uniform float picked;\n\
 uniform vec4  pickedColor;\n\
 uniform vec4  defaultColor;\n\
+uniform float specular;\n\
+uniform float shininess;\n\
+uniform vec3  emission;\n\
 void main() {\n\
     vec3 positionToEyeEC = -v_position; \n\
     vec3 normalEC =normalize(v_normal);\n\
@@ -13,10 +16,10 @@ void main() {\n\
         gl_FragColor = pickedColor;\n\
     }\n\
     czm_material material;\n\
-    material.specular = 0.0;\n\
-    material.shininess = 0.5;\n\
+    material.specular = specular;\n\
+    material.shininess = shininess;\n\
     material.normal =  normalEC;\n\
-    material.emission =vec3(0.2,0.2,0.2);\n\
+    material.emission =emission;//vec3(0.2,0.2,0.2);\n\
     material.diffuse = color.rgb ;\n\
     material.alpha =  color.a;\n\
     gl_FragColor =  czm_phong(normalize(positionToEyeEC), material);\n\
