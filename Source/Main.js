db10d4a67d0a6f1d8cbeb951870a9fcf2720e9c4
MikesWei <695034525@qq.com>
2018-01-24 10:11:30 +0800

    Add files via upload

---
+++ Source/Main.js
@@ -0,0 +1,56 @@
+﻿define([
+    'Core/RendererUtils',
+    'Core/Mesh',
+    'Core/MeshMaterial',
+    'Core/Shaders/ShaderChunk',
+    'Core/MeshVisualizer',
+    'Core/FramebufferTexture',
+    'Core/GeometryUtils',
+    'Core/LOD',
+    'Core/PlaneGeometry',
+    'Core/Rotation',
+    'Core/ReferenceMesh', 
+    'Core/BasicMeshMaterial',
+    'Core/BasicGeometry',
+    'Core/Shaders/ShaderLib',
+    'Core/PlaneBufferGeometry',
+    'Util/CSG'
+], function (
+    RendererUtils,
+    Mesh,
+    MeshMaterial,
+    ShaderChunk,
+    MeshVisualizer,
+    FramebufferTexture,
+    GeometryUtils,
+    LOD,
+    PlaneGeometry,
+    Rotation,
+    ReferenceMesh, 
+    BasicMeshMaterial,
+    BasicGeometry,
+    ShaderLib,
+    PlaneBufferGeometry,
+    CSG
+  ) {
+    if (typeof Cesium==='undefined') {
+        Cesium = {};
+    }
+    Cesium.RendererUtils = RendererUtils;
+    Cesium.Mesh = Mesh;
+    Cesium.MeshMaterial = MeshMaterial;
+    Cesium.ShaderChunk = ShaderChunk;
+    Cesium.ShaderLib = ShaderLib;
+    Cesium.MeshVisualizer = MeshVisualizer;
+    Cesium.FramebufferTexture = FramebufferTexture;
+    Cesium.GeometryUtils = GeometryUtils;
+    Cesium.LOD = LOD;
+    Cesium.PlaneGeometry = PlaneGeometry;
+    Cesium.Rotation = Rotation;
+    Cesium.ReferenceMesh = ReferenceMesh; 
+    Cesium.BasicMeshMaterial = BasicMeshMaterial;
+    Cesium.BasicGeometry = BasicGeometry;
+    Cesium.PlaneBufferGeometry = PlaneBufferGeometry;
+    Cesium.CSG = CSG;
+    return Cesium;
+})
