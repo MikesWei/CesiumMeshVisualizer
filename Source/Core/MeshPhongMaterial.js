262f6e84393b7a9d2f4c8b972fc11a122209cd34
MikesWei <695034525@qq.com>
2018-01-25 06:27:30 +0800

    Update MeshPhongMaterial.js

--- Source/Core/MeshPhongMaterial.js
+++ Source/Core/MeshPhongMaterial.js
@@ -1,11 +1,11 @@
-﻿define([
+define([
     'Core/MeshMaterial',
     'Core/Shaders/phong_frag',
-    'Core/Shaders/phong_vert' 
+    'Core/Shaders/phong_vert'
 ], function (
     MeshMaterial,
     phong_frag,
-    phong_vert 
+    phong_vert
     ) {
     /**
     * 
@@ -14,6 +14,17 @@
     *@extends Cesium.MeshMaterial
     */
     function MeshPhongMaterial(options) {
+        options = options ? options : {};
+
+        options.uniforms = options.uniforms ? options.uniforms : {
+            shininess: -1,
+            emission: [0, 0, 0],
+            specular: 0
+        };
+        options.uniforms.shininess = Cesium.defaultValue(options.uniforms.shininess, 0);
+        options.uniforms.emission = Cesium.defaultValue(options.uniforms.emission, [0.2, 0.2, 0.2]);
+        options.uniforms.specular = Cesium.defaultValue(options.uniforms.specular, 0);
+
         MeshMaterial.apply(this, arguments);
         this.vertexShader = phong_vert;
         this.fragmentShader = phong_frag;
